d8ecd87eac35bfb4f50bfb289d6f98d5805efc4e
EC2 Default User <ec2-user@ip-172-31-9-30.ec2.internal>
2022-03-27 23:42:17 +0000

    lab8

---
+++ lab8.py
@@ -0,0 +1,40 @@
+'''
+lab8 functions
+'''
+
+#8.1
+
+def count_words(input_str):
+    return len(input_str.split())
+# print(count_words('this is a string'))
+
+#8.2
+
+demo_str = 'hwllo world!'
+
+print(count_words(demo_str))
+
+#8.3
+
+def find_min(num_list):
+    
+    min_item = num_list[0]
+    
+    for num in num_list:
+        if type(num) is not str:
+            if min_item >= num:
+             min_item = num
+        
+    return(min_item)
+    
+# print(find_min([1,2,3,4]))
+
+#8.4
+
+demo_list = [1,2,3,4,5,6]
+print(find_min(demo_list))
+
+#3.5
+
+mix_list = [1,2,3,4,'a',5,6]
+print(find_min(mix_list))
